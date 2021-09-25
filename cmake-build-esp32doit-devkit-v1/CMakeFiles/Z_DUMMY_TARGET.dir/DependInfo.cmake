fce6663d2607534e4011e7833c43603fa7bbe720
VictorAlicino <victor.alicino@gmail.com>
2021-09-24 22:54:39 -0300

    Talvez seja um erro enviar esses arquivos mas vai que resolve meu problema

---
+++ cmake-build-esp32doit-devkit-v1/CMakeFiles/Z_DUMMY_TARGET.dir/DependInfo.cmake
@@ -0,0 +1,126 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/victo/CLionProjects/Smart-Blinds/src/main.cpp" "C:/Users/victo/CLionProjects/Smart-Blinds/cmake-build-esp32doit-devkit-v1/CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/config"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/app_trace"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/app_update"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/asio"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/bootloader_support"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/bt"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/coap"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/console"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/driver"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/efuse"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/esp-tls"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/esp32"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/esp_adc_cal"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/esp_event"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/esp_http_client"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/esp_http_server"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/esp_https_ota"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/esp_https_server"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/esp_ringbuf"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/esp_websocket_client"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/espcoredump"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/ethernet"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/expat"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/fatfs"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/freemodbus"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/freertos"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/heap"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/idf_test"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/jsmn"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/json"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/libsodium"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/log"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/lwip"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/mbedtls"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/mdns"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/micro-ecc"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/mqtt"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/newlib"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/nghttp"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/nvs_flash"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/openssl"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/protobuf-c"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/protocomm"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/pthread"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/sdmmc"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/smartconfig_ack"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/soc"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/spi_flash"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/spiffs"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/tcp_transport"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/tcpip_adapter"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/ulp"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/unity"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/vfs"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/wear_levelling"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/wifi_provisioning"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/wpa_supplicant"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/xtensa-debug-module"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/esp-face"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/esp32-camera"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/tools/sdk/include/fb_gfx"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/cores/esp32"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/variants/doitESP32devkitV1"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/ArduinoOTA/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/AsyncUDP/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/AzureIoT/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/BluetoothSerial/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/DNSServer/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/EEPROM/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/ESP32/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/ESPmDNS/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/FFat/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/FS/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/HTTPClient/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/HTTPUpdate/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/HTTPUpdateServer/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/NetBIOS/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/Preferences/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/SD/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/SD_MMC/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/SPIFFS/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/SimpleBLE/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/Ticker/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/Update/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/WiFi/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/WiFiClientSecure/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/WiFiProv/src"
+  "C:/Users/victo/.platformio/packages/framework-arduinoespressif32/libraries/Wire/src"
+  "C:/Users/victo/.platformio/packages/toolchain-xtensa32/xtensa-esp32-elf/include/c++/5.2.0"
+  "C:/Users/victo/.platformio/packages/toolchain-xtensa32/xtensa-esp32-elf/include/c++/5.2.0/xtensa-esp32-elf"
+  "C:/Users/victo/.platformio/packages/toolchain-xtensa32/lib/gcc/xtensa-esp32-elf/5.2.0/include"
+  "C:/Users/victo/.platformio/packages/toolchain-xtensa32/lib/gcc/xtensa-esp32-elf/5.2.0/include-fixed"
+  "C:/Users/victo/.platformio/packages/toolchain-xtensa32/xtensa-esp32-elf/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
